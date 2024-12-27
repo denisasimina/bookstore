472f16ed90d716ef1771e201a2acebcaede02343
Denisa <sdenisa_10@yahoo.com>
2024-12-28 00:54:33 +0200

    footer/Links

--- components/footer/Links.js
+++ components/footer/Links.js
@@ -4,10 +4,10 @@ export default function Links() {
   return (
     <div className={styles.footer__links}>
       {links.map((link) => (
-        <ul>
+        <ul key={index}>
           <b>{link.heading}</b>
           {link.links.map((link) => (
-            <li>
+            <li key={subIndex}>
             <Link href={link.link}>{link.name}</Link>
             </li>
           ))}
