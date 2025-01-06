8f15ef8a0fbc2946e923c77bc83c1a5582d7afed
Denisa <sdenisa_10@yahoo.com>
2025-01-06 18:29:21 +0200

    links

--- components/footer/Links.js
+++ components/footer/Links.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import styles from "./styles.module.scss";
-
 export default function Links() {
   return (
     <div className={styles.footer__links}>
-      {links.map((link, index) => (
-        <ul key={index}> {/* Key pentru fiecare <ul> */}
+      {links.map((link) => (
+        <ul 
+        // key={index}
+        >
           <b>{link.heading}</b>
-          {link.links.map((subLink, subIndex) => ( /* Include subIndex aici */
-            <li key={subIndex}> {/* Key pentru fiecare <li> */}
-              <Link href={subLink.link}>{subLink.name}</Link>
+          {link.links.map((link) => (
+             <li 
+            // key={subIndex}
+            >
+            <Link href={link.link}>{link.name}</Link>
             </li>
           ))}
         </ul>
@@ -17,32 +20,32 @@ export default function Links() {
     </div>
   );
 }
-
 const links = [
-  {
-    heading: "About BookHouse",
-    links: [
-      { name: "About us", link: "#" },
-      { name: "Contact us", link: "#" },
-      { name: "Our Story", link: "#" },
-    ],
-  },
-  {
-    heading: "Help & Support",
-    links: [
-      { name: "FAQ", link: "#" },
-      { name: "Customer Support", link: "#" },
-      { name: "Shipping Info", link: "#" },
-      { name: "Book Recommendations", link: "#" },
-    ],
-  },
-  {
-    heading: "Customer Service",
-    links: [
-      { name: "Return Policy", link: "#" },
-      { name: "Contact Customer Service", link: "#" },
-      { name: "Membership & Rewards", link: "#" },
-      { name: "Gift Cards", link: "#" },
-    ],
-  },
-];
+    {
+      heading: "About BookHouse",
+      links: [
+        { name: "About us", link: "#" },
+        { name: "Contact us", link: "#" },
+        { name: "Our Story", link: "#" },
+      ],
+    },
+    {
+      heading: "Help & Support",
+      links: [
+        { name: "FAQ", link: "#" },
+        { name: "Customer Support", link: "#" },
+        { name: "Shipping Info", link: "#" },
+        { name: "Book Recommendations", link: "#" },
+      ],
+    },
+    {
+      heading: "Customer Service",
+      links: [
+        { name: "Return Policy", link: "#" },
+        { name: "Contact Customer Service", link: "#" },
+        { name: "Membership & Rewards", link: "#" },
+        { name: "Gift Cards", link: "#" }
+      ],
+    },
+  ];
+  
