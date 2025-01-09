f94f42cc7ca327ac9e6ad59874832111201a2699
Denisa <sdenisa_10@yahoo.com>
2025-01-10 00:40:39 +0200

    message provide next remove

--- pages/api/auth/provider.js
+++
@@ -1,6 +0,0 @@
-import { getProviders } from "next-auth/react";
-
-export default async function handler(req, res) {
-  const providers = await getProviders();
-  res.status(200).json(providers);
-}
--- pages/signin.js
+++ pages/signin.js
@@ -224,7 +224,7 @@ if (session) {
   console.log(providers,"KKKKKKKKKKKKKKKKKKKKKKKKK")
   return {
     props: { 
-      providers: providers ? Object.values(providers) : [], 
+      providers: providers , 
       csrfToken, 
       callbackUrl 
     },
