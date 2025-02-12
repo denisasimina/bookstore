1a6a4304d5de0df010cafe4e2cfbf195f53c9287
Denisa <sdenisa_10@yahoo.com>
2025-02-12 13:13:50 +0200

    privacy

---
+++ pages/privacy-policy.js
@@ -0,0 +1,11 @@
+export default function PrivacyPolicy() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Privacy Policy</h1>
+      <p>
+        This is our Privacy Policy. We respect your privacy and do not store personal data
+        beyond authentication purposes.
+      </p>
+    </div>
+  );
+}
