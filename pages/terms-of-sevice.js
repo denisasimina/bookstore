ea1eca6a07cf709a87b9ebe2cea73bc8f5eb1794
Denisa <sdenisa_10@yahoo.com>
2025-02-12 13:19:33 +0200

    privacy

---
+++ pages/terms-of-sevice.js
@@ -0,0 +1,15 @@
+export default function TermsOfUse() {
+    return (
+      <div style={{ padding: "20px" }}>
+        <h1>Terms of Use</h1>
+        <p>
+          By using our service, you agree to the following terms and conditions:
+          <ul>
+            <li>You are responsible for your actions on the site.</li>
+            <li>We are not liable for any damages caused by using our website.</li>
+          </ul>
+        </p>
+      </div>
+    );
+  }
+  
