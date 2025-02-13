737973daa6b039a631e12fea2a1179cd4d6ff871
Denisa <sdenisa_10@yahoo.com>
2025-02-13 17:01:12 +0200

    MAIL

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -13,55 +13,44 @@ const {
 } = process.env;
 
 const oauth2Client = new OAuth2(
-  MAILING_SERVICE_CLIENT_ID,
-  MAILING_SERVICE_CLIENT_SECRET,
-  MAILING_SERVICE_CLIENT_REFRESH_TOKEN,
+  NEXT_PUBLIC_MAILING_SERVICE_CLIENT_ID,
+  NEXT_PUBLIC_MAILING_SERVICE_CLIENT_SECRET,
+  NEXT_PUBLIC_MAILING_SERVICE_CLIENT_REFRESH_TOKEN,
   OAUTH_PLAYGROUND
 );
 
-
-
-export const sendEmail = async (to, url, txt, subject,template) => {
-
-
+export const sendEmail = async (to, url, txt, subject, template) => {
   oauth2Client.setCredentials({
-    refresh_token: MAILING_SERVICE_CLIENT_REFRESH_TOKEN,
+    refresh_token: NEXT_PUBLIC_MAILING_SERVICE_CLIENT_REFRESH_TOKEN,
   });
 
-
-
   const accessToken = await oauth2Client.getAccessToken();
-
 
   const smtpTransport = nodemailer.createTransport({
     service: "gmail",
     auth: {
       type: "OAuth2",
-      user: SENDER_EMAIL_ADDRESS,
-      clientId: MAILING_SERVICE_CLIENT_ID,
-      clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-      refreshToken: MAILING_SERVICE_CLIENT_REFRESH_TOKEN,
+      user: NEXT_PUBLIC_SENDER_EMAIL_ADDRESS,
+      clientId: NEXT_PUBLIC_MAILING_SERVICE_CLIENT_ID,
+      clientSecret: NEXT_PUBLIC_MAILING_SERVICE_CLIENT_SECRET,
+      refreshToken: NEXT_PUBLIC_MAILING_SERVICE_CLIENT_REFRESH_TOKEN,
       accessToken: accessToken.token,
     },
   });
 
-
-
   const mailOptions = {
-    from: SENDER_EMAIL_ADDRESS,
+    from: NEXT_PUBLIC_SENDER_EMAIL_ADDRESS,
     to: to,
     subject: subject,
     html: template(to, url),
   };
 
-
-
   smtpTransport.sendMail(mailOptions, (err, info) => {
     if (err) return err;
     return info;
   });
-  
 };
+
 export const sendNewsletter = async (recipientEmail) => {
   const emailSubject = "Confirm Your Subscription to Our Newsletter";
 
@@ -79,4 +68,5 @@ export const sendNewsletter = async (recipientEmail) => {
     console.log("Confirmation email sent to:", recipientEmail);
   } catch (err) {
     console.error("Error sending confirmation email:", err);
-  }}
+  }
+};
